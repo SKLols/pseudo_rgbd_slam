0894182756c361261c63a50796cd03205daae37c
“SKLols” <sourabha.lolage@gmail.com>
2026-05-25 15:23:36 +0200

    Node A complete: reads TUM dataset, publishes 613 RGB frames to /camera/rgb/image_raw at 30fps

--- pseudo_rgbd_slam/pseudo_rgbd_slam/node_a_broadcaster.py
+++ pseudo_rgbd_slam/pseudo_rgbd_slam/node_a_broadcaster.py
@@ -8,6 +8,9 @@ import cv2
 class RGBBrodcaster ( Node ):
     def __init__(self):
         super().__init__('node_a_broadcaster')
+        self.get_logger().info('Node A started')
+
+        #Load dataset  
         self.dataset_path = '/home/ubuntu2204/datasets/tum/rgbd_dataset_freiburg1_desk'
         self.get_logger().info(f'Path_exists: {os.path.exists(self.dataset_path)}')        
         self.get_logger().info(f'Dataset_path:{self.dataset_path}')
@@ -23,7 +26,36 @@ class RGBBrodcaster ( Node ):
             self.get_logger().info(f'First frame: {self.frame_list[0]}')   
             self.get_logger().info(f'Last frame: {self.frame_list[-1]}')
         
-        self.get_logger().info('Node A started')
+        self.publisher = self.create_publisher(Image, '/camera/rgb/image_raw', 10)
+        self.timer = self.create_timer(1/30, self.timer_callback)
+        self.frame_idx = 0
+
+        self.bridge = CvBridge()
+
+    def timer_callback(self):
+        if self.frame_idx >= len(self.frame_list):
+            self.get_logger().info('All frames published, stopping timer')
+            self.timer.cancel()
+            return
+        
+        current_frame = self.frame_list[self.frame_idx]
+        timestamp, filepath = current_frame
+        absolute_path = os.path.join(self.dataset_path, filepath)
+
+        #read the image using OpenCV
+        image = cv2.imread(absolute_path)
+        if image is None:
+            self.get_logger().error(f'Failed to read image at {absolute_path}')
+            self.frame_idx += 1
+            return
+
+        #convert to ROS Image message
+        msg = self.bridge.cv2_to_imgmsg(image, encoding = 'bgr8')
+
+        #publish the message
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Published frame {self.frame_idx}: {absolute_path}')
+        self.frame_idx += 1
 
     def _load_rgb_list(self):
         rgb_txt = os.path.join(self.dataset_path, 'rgb.txt')
